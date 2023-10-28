e7afcf84c1ab9237859925a2de851b999851a93e
hoangthien2003 <hoangdangbaothien@gmail.com>
2023-10-28 20:50:56 +0700

    Done: Auth, author API v1.1

--- Entities/Convocation2023Context.cs
+++ Entities/Convocation2023Context.cs
@@ -25,7 +25,7 @@ public partial class Convocation2023Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=Convocation2023;Integrated Security=True;Trusted_Connection=true;Encrypt=false");
+        => optionsBuilder.UseSqlServer("Data Source=34.101.233.31;Initial Catalog=Convocation2023;User ID=sa;Password=<YourStrong@Passw0rd>;TrustServerCertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
