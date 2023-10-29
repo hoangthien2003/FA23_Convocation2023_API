12b09f2ced6a0c98c995ae30360ec09bbd0c0f9d
hoangthien2003 <hoangdangbaothien@gmail.com>
2023-10-29 18:19:07 +0700

    Update: FullName utf8 property

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Text.Json.Serialization;
 using Microsoft.Extensions.Configuration;
 using FA23_Convocation2023_API.Hubs;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FA23_Convocation2023_API
 {
@@ -17,6 +21,7 @@ namespace FA23_Convocation2023_API
         {
 
             var builder = WebApplication.CreateBuilder(args);
+            
             // Add services to the container.
             builder.Services.AddControllers();
             builder.Services.AddControllers().AddJsonOptions(options =>
@@ -88,7 +93,6 @@ namespace FA23_Convocation2023_API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-            
 
             // Add Cors 1 dòng :))))
             app.UseCors();
